9b4958852a57139ac7450d9abc9fbe454d7d5bfd
b.saintot <bsaintot@octo.com>
2020-01-31 18:46:05 +0100

    Working streamlit app

--- app/display_predictions.py
+++ app/display_predictions.py
@@ -4,8 +4,11 @@ import streamlit as st
 st.title('OCTO Fly')
 st.markdown('Welcome to the new OCTO Fly app!')
 
+predictions = pd.read_csv('./data/processed/predictions.csv')
+predictions_placeholder = st.dataframe(predictions)
+
 button = st.button("Retrieve last predictions")
 if button:
     st.balloons()
     predictions = pd.read_csv('./data/processed/predictions.csv')
-    st.dataframe(predictions)
+    predictions_placeholder.dataframe(predictions)
