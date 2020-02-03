d679e8ad5bfdcfdd7dd335c4b0492cb41cb23cc9
b.saintot <bsaintot@octo.com>
2020-02-03 17:42:24 +0100

    Package application

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='OCTO Fly',
+    name='octo_fly',
     author='BASA',
     author_email='basa@octo.com',
     version='0.1.0',
     description='OCTO Fly project created for the AI certification',
     packages=find_packages(),
     python_requires='~=3.7',
-    install_requires=['pandas', 'numpy'],
+    install_requires=['pandas',
+                      'numpy',
+                      'prefect',
+                      'streamlit',
+                      'sklearn'],
     extras_require={'tests': ['pytest']}
 )
