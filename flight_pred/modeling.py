f5a736bb93ab74eefdd528cb5c8a81fa876308cf
b.saintot <bsaintot@octo.com>
2020-02-05 13:04:36 +0100

    Change delay threshold to 15min

--- flight_pred/modeling.py
+++ flight_pred/modeling.py
@@ -6,29 +6,29 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-from config import PROJECT_HOME, TARGET_COLUMN, ID_COLUMN
+from config import PROJECT_HOME, TARGET_COLUMN, ID_COLUMN, DELAY_THRESHOLD
 
 
 @task
 def train_and_evaluate(flights: pd.DataFrame) -> pickle:
     X = flights.drop(columns=[TARGET_COLUMN, ID_COLUMN])
-    y = flights[TARGET_COLUMN].map(lambda x: 1 if x > 0 else 0)
+    y = flights[TARGET_COLUMN].map(lambda x: 1 if x > DELAY_THRESHOLD else 0)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
     model = RandomForestClassifier(n_estimators=10, max_depth=10, n_jobs=-1, verbose=2, random_state=42)
     model.fit(X_train, y_train)
-    pickle.dump(model, open(PROJECT_HOME/'models/model_0.pkl', 'wb'))
+    pickle.dump(model, open(PROJECT_HOME/f'models/model_{DELAY_THRESHOLD}min.pkl', 'wb'))
 
     y_predicted = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_predicted.round())
-    print(f'Accuracy: {accuracy}')
+    print(f'Test accuracy: {accuracy}')
 
     return model
 
 
 @task
 def get_predictions(flights: pd.DataFrame) -> pd.DataFrame:
-    model = pickle.load(open(PROJECT_HOME/'models/model_0.pkl', 'rb'))
+    model = pickle.load(open(PROJECT_HOME/'models/model_15min.pkl', 'rb'))
     predictions = model.predict_proba(flights.drop(columns=[ID_COLUMN]))
     flights['prediction'] = [prediction[1] for prediction in predictions]
     return flights[[ID_COLUMN, 'prediction']]
--- flight_pred/retrieve.py
+++ flight_pred/retrieve.py
@@ -8,13 +8,14 @@ from prefect import task
 def get_flights_data(db_1_path: str, db_2_path: str, workflow_mode: str = 'train') -> pd.DataFrame:
     conn_1 = sqlite3.connect(db_1_path)
     conn_2 = sqlite3.connect(db_2_path)
+
     flights_1 = pd.read_sql_query("SELECT * FROM vols", conn_1)
     flights_2 = pd.read_sql_query("SELECT * FROM vols", conn_2)
-
     flights = pd.concat([flights_1, flights_2])
 
-    # TODO to simulate predictions
+    # FIXME to simulate predictions
     if workflow_mode == 'predict':
+        flights = flights.sort_values(['DATE', 'DEPART PROGRAMME'], ascending=[True, True])
         flights = flights.tail(50)
 
     return flights
